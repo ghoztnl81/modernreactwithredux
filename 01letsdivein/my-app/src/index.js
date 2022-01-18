c54befa3020ea1d9e1a7167b5aec33a6e17e0c67
Ferdy van den Berg <fer@ghozt.nl>
2022-01-18 10:13:41 +0000

    Update 18-01-22

--- 01letsdivein/my-app/src/index.js
+++ 01letsdivein/my-app/src/index.js
@@ -1,17 +1,14 @@
+// Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Create React component
+const App = () => {
+    return <div>Hi there!</div>;
+}
+
+// Take the React component and show it on screen.
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

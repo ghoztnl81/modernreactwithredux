637c2533d706ad8249cab2f0a63f7333644d99e1
Ferdy van den Berg <fer@ghozt.nl>
2022-01-18 14:38:15 +0000

    Update 18-01-22: halfway in module2

--- 01letsdivein/my-app/src/index.js
+++ 01letsdivein/my-app/src/index.js
@@ -2,9 +2,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click on me!';
+}
+
 // Create React component
 const App = () => {
-    return <div>Hi there!</div>;
+    
+    return (
+        <div>
+            <label className="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor:'blue', color: 'white'}}>
+                {getButtonText()}
+            </button>
+        </div>
+    );
 }
 
 // Take the React component and show it on screen.

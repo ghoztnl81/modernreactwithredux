c722b34afa384c347c5990c498314f605ae7c984
Ferdy van den Berg <fer@ghozt.nl>
2022-01-20 12:08:20 +0000

    Update 20-01-22: Starting with components

--- 01letsdivein/my-app/src/index.js
+++ 01letsdivein/my-app/src/index.js
@@ -2,21 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-    return 'Click on me!';
-}
-
 // Create React component
 const App = () => {
-    
+    const buttonText = { text: 'Click me!' };
     return (
         <div>
-            <label className="label" for="name">
+            <label className="label" htmlFor="name">
                 Enter name:
             </label>
             <input id="name" type="text"/>
             <button style={{backgroundColor:'blue', color: 'white'}}>
-                {getButtonText()}
+                {buttonText.text}
             </button>
         </div>
     );
